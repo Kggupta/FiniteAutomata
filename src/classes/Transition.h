7b08b0546aaa1de723162b0d97a765daf51968a6
Kggupta <33162944+Kggupta@users.noreply.github.com>
2022-05-18 17:35:33 -0400

    Add transition class

---
+++ src/classes/Transition.h
@@ -0,0 +1,21 @@
+#ifndef __TRANSITION_H
+#define __TRANSITION_H
+#include <string>
+using std::string;
+class State;
+
+class Transition {
+    State & src;
+    char read;
+    State & dest;
+    public:
+    Transition(State & src, char read, State & dest);
+    State & getDestination() const;
+    char getRead() const;
+    State & getSource() const;
+};
+
+bool operator== (const Transition &left, const Transition &right);
+
+
+#endif
