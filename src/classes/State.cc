d9012ad0c8a0c76bad1785ae021467a2d38d1829
Kggupta <33162944+Kggupta@users.noreply.github.com>
2022-05-19 17:58:56 -0400

    State constructors

--- src/classes/State.cc
+++ src/classes/State.cc
@@ -4,7 +4,9 @@ string State::getName() const {return name;}
 
 bool State::isAccept() const {return accepting;}
 
-State::State(string name, bool accept = false): name(name), accepting(accept) {}
+State::State(string name, bool accept): name(name), accepting(accept) {}
+
+State::State(string name): name(name), accepting(false) {}
 
 void State::setAccepting(bool accept) {accepting = accept;}
 
